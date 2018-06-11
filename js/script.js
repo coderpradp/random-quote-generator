8b5c018fa3d2573038f030077fc44caa064d4e49
Pradip Khadka <fnfdrifter@gmail.com>
2018-06-11 14:35:43 +0545

    Setup quotes api and finalize

--- js/script.js
+++ js/script.js
@@ -1,15 +1,3 @@
-function ready(fn) {
-  if (
-    document.attachEvent
-      ? document.readyState === 'complete'
-      : document.readyState !== 'loading'
-  ) {
-    fn()
-  } else {
-    document.addEventListener('DOMContentLoaded', fn)
-  }
-}
-
 var colors = [
   '#16a085',
   '#27ae60',
@@ -27,94 +15,77 @@ var colors = [
 var currentQuote = ''
 var currentAuthor = ''
 
-window.processQuote = function(response) {
-  console.log(response)
+function success(response) {
+  // console.log('Response', response)
+  var r = response
+  currentQuote = $(r[0].content).text()
+  currentAuthor = r[0].title
+  // console.log('Current Quote', currentQuote)
+  // console.log('Current Author', currentAuthor)
+  var color = Math.floor(Math.random() * colors.length)
+  var tweetUrl = 'https://twitter.com/intent/tweet?hashtags=quotes&text='
+  $('.quote-text').animate(
+    {
+      opacity: 0
+    },
+    500,
+    function() {
+      $(this).animate(
+        {
+          opacity: 1
+        },
+        500
+      )
+      $('#text').text(currentQuote)
+    }
+  )
+  $('.quote-author').animate(
+    {
+      opacity: 0
+    },
+    500,
+    function() {
+      $(this).animate(
+        {
+          opacity: 1
+        },
+        500
+      )
+      $('#author').text(currentAuthor)
+    }
+  )
+  $('html body').animate(
+    {
+      backgroundColor: colors[color],
+      color: colors[color]
+    },
+    1000
+  )
+  $('.button').animate(
+    {
+      backgroundColor: colors[color]
+    },
+    1000
+  )
+  $('#tweet-quote').attr(
+    'href',
+    tweetUrl + '"' + currentQuote + '" - ' + currentAuthor
+  )
 }
 
 function getQuote() {
-  var url =
-    'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_jsonp=processQuote'
-  var script = document.createElement('script')
-  script.setAttribute('src', url)
-  document.body.appendChild(script)
+  $.ajax({
+    url:
+      'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_jsonp=?',
+    dataType: 'jsonp',
+    jsonpCallback: 'success'
+  })
 }
 
-// function getQuote() {
-//   $.ajax({
-//     headers: {
-//       'X-Mashape-Key': '1x5ZG6L8Z9mshWMZS92wfBuoAKIJp1VBhHqjsnIidiTOyzsKSz',
-//       Accept: 'application/json',
-//       'Content-Type': 'application/x-www-form-urlencoded'
-//     },
-//     url: 'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous',
-//     success: function(response) {
-//       // console.log(response)
-//       // var r = JSON.parse(response);
-//       var r = response
-//       currentQuote = r.quote
-//       currentAuthor = r.author
-//       var color = Math.floor(Math.random() * colors.length)
-//       var tweetUrl =
-//         'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text='
-
-//       $('.quote-text').animate(
-//         {
-//           opacity: 0
-//         },
-//         500,
-//         function() {
-//           $(this).animate(
-//             {
-//               opacity: 1
-//             },
-//             500
-//           )
-//           $('#text').text(currentQuote)
-//         }
-//       )
-
-//       $('.quote-author').animate(
-//         {
-//           opacity: 0
-//         },
-//         500,
-//         function() {
-//           $(this).animate(
-//             {
-//               opacity: 1
-//             },
-//             500
-//           )
-//           $('#author').text(currentAuthor)
-//         }
-//       )
-
-//       $('html body').animate(
-//         {
-//           backgroundColor: colors[color],
-//           color: colors[color]
-//         },
-//         1000
-//       )
-//       $('.button').animate(
-//         {
-//           backgroundColor: colors[color]
-//         },
-//         1000
-//       )
-
-//       $('#tweet-quote').attr(
-//         'href',
-//         tweetUrl + '"' + currentQuote + '" - ' + currentAuthor
-//       )
-//     } //end of success
-//   }) //end of ajax
-// } //end of getQuote
-
-ready(function() {
+$(document).ready(function() {
   getQuote()
 
-  document.getElementById('new-quote').onclick = function() {
+  $('#new-quote').click(function() {
     getQuote()
-  }
+  })
 })
